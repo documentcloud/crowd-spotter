afe7b679613831dc36dacc47390239678aa54291
Nathan Stitt <nathan@stitt.org>
2014-06-11 17:00:38 -0500

    Serve js, scss and html

--- lib/crowd/spotter/application.rb
+++ lib/crowd/spotter/application.rb
@@ -1,22 +1,37 @@
 require "sinatra"
 require 'oj'
+require 'pathname'
+require 'sass'
+require 'compass'
 
 module Crowd
   module Spotter
     class Application < Sinatra::Base
 
       configure do
+        set :root, Pathname.new(__FILE__).dirname.join('../../../')
         Crowd::Spotter.startup
+        
+        Compass.configuration do |config|
+          config.project_path = root #.join('public/css')
+        end
+
       end
 
       get '/' do
-        "Hello, world!"
+        send_file File.join(settings.public_folder, 'index.html')
       end
 
       get '/statistics' do
         content_type 'application/json'
         Oj.dump(Spotter.statistics.to_hash, mode: :compat)
       end
+
+      get '/css/:name.css' do
+        content_type 'text/css', :charset => 'utf-8'
+        scss(:"css/#{params[:name]}", Compass.sass_engine_options )
+      end
+
 
     end
   end
