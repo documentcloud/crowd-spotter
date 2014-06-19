b54b1e967f65c66d80ac07040804d0284487c274
Nathan Stitt <nathan@stitt.org>
2014-06-19 13:02:24 -0500

    Integrate jammit for serving assets in production

--- crowd-spotter.gemspec
+++ crowd-spotter.gemspec
@@ -22,6 +22,8 @@ Gem::Specification.new do |spec|
   spec.add_dependency "celluloid", "~>0.15.2"
   spec.add_dependency "sinatra", ">= 0.7.3"
   spec.add_dependency "oj"
+  spec.add_dependency "jammit-sinatra"
+
 
   spec.add_development_dependency "bundler", "~> 1.5"
   spec.add_development_dependency "rake"
--- lib/crowd/spotter/application.rb
+++ lib/crowd/spotter/application.rb
@@ -3,23 +3,38 @@ require 'oj'
 require 'pathname'
 require 'sass'
 require 'compass'
+require 'jammit/sinatra'
+require 'jammit'
+require 'pry'
 
 module Crowd
   module Spotter
     class Application < Sinatra::Base
 
+      register Jammit
+
       configure do
         set :root, Pathname.new(__FILE__).dirname.join('../../../')
+        Jammit.load_configuration(root.join('config','assets.yml'))
         Crowd::Spotter.startup
-        
         Compass.configuration do |config|
-          config.project_path = root #.join('public/css')
+          config.project_path = root
         end
+        Jammit.set_package_assets( ENV["RACK_ENV"]=="production" )
+      end
 
+      helpers do
+        # Needed for Jammit until
+        # https://github.com/documentcloud/jammit/commit/dcc9b02c2b9254fc4e6e53ba4a2dbc36378540cd
+        def javascript_include_tag(*sources,options)
+          sources.map{ |source|
+            "<script src=\"#{source}\" type=\"application/javascript\"></script>"
+          }.join("\n")
+        end
       end
 
       get '/' do
-        send_file File.join(settings.public_folder, 'index.html')
+        erb :index
       end
 
       get '/statistics' do
@@ -36,7 +51,6 @@ module Crowd
         content_type 'text/css', :charset => 'utf-8'
         scss(:"css/#{params[:name]}", Compass.sass_engine_options )
       end
-
 
     end
   end
